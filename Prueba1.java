19c9a26d6d5ff9c45e0ba63bf2951baa0b87e9f3
Diego <garridod563@gmail.com>
2024-09-24 03:41:12 -0400

    He anadido mi lugar favorito

--- Prueba1.java
+++ Prueba1.java
@@ -1,7 +1,7 @@
-public class Prueba {
+public class Prueba1 {
 	public static void main(String[] args) {
 		String deporteFavorito = "Culturismo";
 		System.out.println("Mi deporte favorito es: Culturismo");
-
+		System.out.println("Mi lugar favorito es el gimnasio");
 	}
 }
