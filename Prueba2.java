4abb49da8e4704ebc36c560894e05d0884b79c3a
Diego <garridod563@gmail.com>
2024-09-24 03:45:46 -0400

    He agregado mi red social favorita

---
+++ Prueba2.java
@@ -0,0 +1,8 @@
+public class Prueba2 {
+	public static void main(String[] args) {
+		String deporteFavorito = "Culturismo";
+		System.out.println("Mi deporte favorito es: Culturismo");
+		System.out.println("Mi lugar favorito es el gimnasio");
+		System.out.println("Mi red social favorita es Instagram");
+	}
+}
